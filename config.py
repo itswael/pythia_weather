bd6f3f176dfb3c8879cf277db80704d0c3b8860c
Mohammad Wael <errwael@gmail.com>
2025-10-10 11:01:57 -0400

    updated constant for API base url

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 
 
-NASA_POWER_BASE = "https://power.larc.nasa.gov/api/temporal/daily/point"
+NASA_POWER_API_BASE = "https://power.larc.nasa.gov/api/temporal/daily/point"
 NASA_POWER_S3_BASE = "https://nasa-power.s3.us-west-2.amazonaws.com/"
 
 # Known daily Zarr roots (LST) for POWER ARD on AWS S3 (public/anonymous)
@@ -15,6 +15,8 @@ MERRA2DAILY_ZARR_HINT = (
     "https://nasa-power.s3.us-west-2.amazonaws.com/"
     "merra2/temporal/power_merra2_daily_temporal_lst.zarr"
 )
+# API Parameters
+NASA_POWER_API_PARAMS = "T2M_MAX,T2M_MIN,PRECTOTCORR,ALLSKY_SFC_SW_DWN"
 
 # Default variable sets
 SOLAR_VARS = ["ALLSKY_SFC_SW_DWN"]  # SRAD source (W m^-2) -> convert to MJ m^-2 d^-1
